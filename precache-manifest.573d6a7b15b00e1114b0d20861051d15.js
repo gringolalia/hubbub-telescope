6f2b60d3f4f9f9b54e474a353939ec28c87c9a43
Colin Brayton <gringolalia@gmail.com>
2020-03-06 13:00:42 -0300

    deploy

---
+++ precache-manifest.573d6a7b15b00e1114b0d20861051d15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdedbc07affefd6b100a",
+    "url": "/hubbub-telescope/js/about.050a23c7.js"
+  },
+  {
+    "revision": "97a7e5391a498c85f7ed",
+    "url": "/hubbub-telescope/js/app.0911898e.js"
+  },
+  {
+    "revision": "ee4574dd27db9d99d28c",
+    "url": "/hubbub-telescope/css/chunk-vendors.d063ddc7.css"
+  },
+  {
+    "revision": "ee4574dd27db9d99d28c",
+    "url": "/hubbub-telescope/js/chunk-vendors.71cbd31d.js"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/hubbub-telescope/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "0758d0d3098604c5750637eebd38efd8",
+    "url": "/hubbub-telescope/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hubbub-telescope/robots.txt"
+  }
+];
